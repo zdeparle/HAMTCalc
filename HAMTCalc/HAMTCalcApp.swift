a1ccf8e3023a647eba3ff1b0c91827ddddb5cd0f
Zachary DeParle <zachary.deparle@duke.edu>
2024-01-08 22:11:39 -0500

    Initial Commit

---
+++ HAMTCalc/HAMTCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAMTCalcApp.swift
+//  HAMTCalc
+//
+//  Created by Zachary DeParle on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HAMTCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
